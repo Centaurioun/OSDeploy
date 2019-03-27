9af21326684da4c58ed7afa8db02d5d640fb74d1
David Segura <david@segura.org>
2019-03-26 23:07:25 -0500

    19.1.15.0

---
+++ OSDeploy.psm1
@@ -0,0 +1,13 @@
+﻿<#	
+	===========================================================================
+	 Author:   		David Segura
+	 Organization: 	OSDeploy.com
+	 Filename:     	OSDeploy.psm1
+	 Version:   	18.06.15
+	 --------------------------------------------------------------------------
+	 Module Name: OSDeploy
+	===========================================================================
+#>
+. $PSScriptRoot\Copy-IsoToUsb.ps1
+. $PSScriptRoot\New-CAB.ps1
+. $PSScriptRoot\Show-RegistryXML.ps1
